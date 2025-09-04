6856c35c47435cf852ef6c52780182bd405316ab
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-09-04 19:27:33 -0300

    prática de verificação de senha e adição de classe

---
+++ aula04/pratica04js/verificacao.js
@@ -0,0 +1,19 @@
+function verificarSenha(event) {
+  event.preventDefault();
+  let user = document.getElementById("user").value;
+  let senha = document.getElementById("senha").value;
+
+  if (user === "user" && senha === "pass") 
+  {
+    alert("Login OK");
+    document.querySelectorAll(".dado_login").forEach(div => {
+      div.classList.remove("user_pass_fail")
+    });
+  }
+  else
+  {
+    document.querySelectorAll(".dado_login").forEach(div => {
+      div.classList.add("user_pass_fail")
+    });
+  }
+}
