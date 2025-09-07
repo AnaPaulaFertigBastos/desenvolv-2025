1314ec3daff62b812cb84bbf6868c5ce63b19136
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-09-07 14:58:48 -0300

    cálculos de +, -, /, * e **. Definição do fundo do input baseado no resultado

---
+++ aula05/pratica01/calculadora.js
@@ -0,0 +1,94 @@
+function getValor(){
+  let valor1 = parseFloat(document.getElementById("valor1").value);
+  let valor2 = parseFloat(document.getElementById("valor2").value);
+
+  if (isNaN(valor1)) {
+    valor1 = 0;
+  } 
+  
+  if (isNaN(valor2)) {
+    valor2 = 0;
+  }
+
+  return [valor1, valor2];
+}
+
+function soma(){
+  let [valor1, valor2] = getValor();
+  let resultado = valor1 + valor2;
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function subtracao(){
+  let [valor1, valor2] = getValor();
+  let resultado = valor1 - valor2;
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function multiplicacao(){
+  let [valor1, valor2] = getValor();
+  let resultado = valor1 * valor2;
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function divisao(){
+  let [valor1, valor2] = getValor();
+  let resultado = valor1 / valor2;
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function exponenciacao(){
+  let [valor1, valor2] = getValor();
+  let resultado = valor1 ** valor2;
+
+  document.getElementById("resultado").value = resultado;
+}
+
+function definirCss() {
+  let resultado = parseFloat(document.getElementById("resultado").value);
+  let inputResultado = document.getElementById("resultado");
+
+  if (resultado > 0) {
+    inputResultado.classList.remove("resultado-negativo");
+    inputResultado.classList.remove("resultado-neutro");
+    inputResultado.classList.add("resultado-positivo");
+  }
+  else if (resultado < 0) {
+    inputResultado.classList.remove("resultado-positivo");
+    inputResultado.classList.remove("resultado-neutro");
+    inputResultado.classList.add("resultado-negativo");
+  }
+  else {
+    inputResultado.classList.remove("resultado-positivo");
+    inputResultado.classList.remove("resultado-negativo");
+    inputResultado.classList.add("resultado-neutro");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("soma").addEventListener("click", () => {
+    soma();
+    definirCss();
+  });
+  document.getElementById("subtracao").addEventListener("click", () => {
+    subtracao();
+    definirCss();
+  });
+  document.getElementById("multiplicacao").addEventListener("click", () => {
+    multiplicacao();
+    definirCss();
+  });
+  document.getElementById("divisao").addEventListener("click", () => {
+    divisao();
+    definirCss();
+  });
+  document.getElementById("exponenciacao").addEventListener("click", () => {
+    exponenciacao();
+    definirCss();
+  });
+
+})
