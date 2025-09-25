aa96f646b42f6f62d99580544a852e9e34e34ccc
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-09-25 20:00:48 -0300

    acréscimo e cálculo de salários

---
+++ aula08/pratica02/index.php
@@ -0,0 +1,13 @@
+<?php 
+  $SALARIO1 = 1000;
+  $SALARIO2 = 2000;
+
+  $SALARIO2 = $SALARIO1;
+
+  $SALARIO2++;
+
+  $SALARIO1 = $SALARIO1 * 1.10;
+
+  echo("Valor salário 1:" . $SALARIO1 . "<br>" . "Valor salário 2: " . $SALARIO2);
+
+?>
