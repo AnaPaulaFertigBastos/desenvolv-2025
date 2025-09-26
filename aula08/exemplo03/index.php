13575ab44f5f474c4722d558679c3a3d58ae6083
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-09-25 21:15:42 -0300

    matriz associativa

--- aula08/exemplo03/index.php
+++ aula08/exemplo03/index.php
@@ -3,4 +3,18 @@
   foreach($nomes as $elemento) {
     echo "Nome: " . $elemento . "<br>";
   }
+
+  //Inicializando valor em vetor a partir de sua posição
+  
+  $valores[0] = "UNI";
+  $valores[1] = "DAVI";
+  $valores[2] = true;
+
+  //Matriz asssociativa
+
+  $idade = array("João"=>"35", "Maria"=>"37", "José"=>"43");
+  foreach($idade as $chave => $valor) {
+    echo "Chave=" . $chave . ", Valor =" . $valor;
+    echo "<br>";
+  }
 ?>
