d8b0401932b2f5b8a4a73890bcce51ca6880cea9
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-09-25 21:30:40 -0300

    utilizando arrays

--- aula08/pratica05/index.php
+++ aula08/pratica05/index.php
@@ -1,3 +1,15 @@
 <?php 
+  $materias = array("Estrutura de Dados II", "Engenharia de Software II", "Administração e Sistemas", "Programação Web", "Banco de Dados II");
 
+  $professores = array("Fernando", "Jullian", "Marciel", "Cleber", "Marco");
+  for ($i = 0; $i < count($materias); $i++) {
+    echo "Disciplina: " . $materias[$i] . ", Professor: " . $professores[$i] . "<br>";
+  };
+
+  echo "<br>Com associativa:";
+  $materiasEProfe = array("Estrutura de Dados II"=>"Fernando", "Engenharia de Software II"=>"Jullian", "Administração e Sistemas"=>"Marciel", "Programação Web"=>"Cleber", "Banco de Dados II"=>"Marco");
+
+  foreach($materiasEProfe as $materia => $professor) {
+    echo "$materia - $professor <br>";
+  };
 ?>
