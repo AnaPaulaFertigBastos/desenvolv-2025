be46bec75dac37ea9b5d31b74ff5a89957fae714
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-10-02 21:18:45 -0300

    try catch finally

--- aula09/pratica03/index.php
+++ aula09/pratica03/index.php
@@ -5,14 +5,21 @@
   $notasPorAluno = [
         "Maria" => [10, 8, 7, 9, 7],
         "João"  => [7, 5, 4, 9, 5]
-    ];
+  ];
 
-    $faltasPorAluno = [
-        "Maria" => [0, 1, 0, 0, 0],
-        "João"  => [1, 1, 1, 0, 0]
-    ];
+  $faltasPorAluno = [
+      "Maria" => [0, 1, 0, 0, 0],
+      "João"  => [1, 1, 1, 0, 0]
+  ];
 
-  mediaPorAluno($notasPorAluno);
-  frequenciaAluno($faltasPorAluno);
+  try {
+    mediaPorAluno($notasPorAluno);
+    frequenciaAluno($faltasPorAluno);
+  } catch(Exception $e) {
+    exibirMensagem("Erro: " . $e->getMessage());
+  } finally {
+    exibirMensagem("Processamento finalizado!");
+  }
+  
 
 ?>
