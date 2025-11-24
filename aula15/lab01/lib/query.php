fd059e51ea2109b00429046ef40719fd51cdca78
AnaPaulaFertigBastos <anapfb25@gmail.com>
2025-11-23 22:31:19 -0300

    ajustes

--- aula15/lab01/lib/query.php
+++ aula15/lab01/lib/query.php
@@ -1,32 +1,72 @@
 <?php 
-  class query {
-    private $sql;
-    private $registros;
-    private $conexaoBd;
-    private $queryResource;
 
-    public function __construct($conexaoBd) {
-      $this->registros = 0;
-      $this->conexaoBd = $conexaoBd;
-    }
+    require_once 'conexaobd.php';
 
-    public function setSql($sSql) {
-      $this->sql = $sSql;
-    }
+    class query {
+        private $sql;
+        private $registros;
+        private $connection;
+        private $queryResource;
 
-    public function open() {
-      $retorno = pg_query($this->conexaoBd->getInternalConnection(), $this->sql);
-      if ($retorno) {
-        $this->registros = pg_num_rows($query);
-        $this->queryResource = queryResource;
-        return true;
-      }
-    }
+        public function __construct($oConn) {
+            $this->registros = 0;
+            $this->connection = $oConn;
+        }
 
-    public function getNextRow() {
-      $linha = pg_fetch_assoc($this->queryResource);
-      return $linha;
+        public function open() {
+            $this->queryResource = pg_query($this->connection->getInternalConnection(), $this->sql);
+            if($this->queryResource) {
+                //Retorna a quantidade de linhas da query.
+                $this->registros = pg_num_rows($this->queryResource);
+                return true;
+            }
+            return false;
+        }
+
+        public function getNextRow() {
+            $row = pg_fetch_assoc($this->queryResource);
+            if($row) {
+                return $row;
+            }
+            return false;
+        }
+
+        public function update($stabela, $aColunas, $aValores, $sWhere) {
+
+            // Montar "col1 = $1, col2 = $2, col3 = $3 ..."
+            $set = [];
+            for ($i = 0; $i < count($aColunas); $i++) {
+                $set[] = $aColunas[$i] . " = $" . ($i + 1);
+            }
+
+            $setSql = implode(", ", $set);
+
+            $sql = "UPDATE $stabela SET $setSql WHERE $sWhere";
+
+            return pg_query_params(
+                $this->connection->getInternalConnection(),
+                $sql,
+                $aValores
+            );
+        }
+
+
+        public function insert($sTabela, $aColunas, $aValores) {
+            //TODO implementar método de insert
+        }
+
+        public function delete($sTabela, $sWhere) {
+            //TODO implementar método de delete
+        }
+
+        public function getRegistros() {
+            return $this->registros;
+        }
+
+        public function setSql($sSql) {
+            $this->sql = $sSql;
+        }
+
     }
-  }
 
 ?>
